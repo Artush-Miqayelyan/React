f0303d98218e58faedecf8c1cfc9dd200c69c64b
Artush-Miqayelyan <miqaelyan.artush@mail.ru>
2024-02-21 23:29:25 +0400

    change state on reducer

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,64 +1,80 @@
-import { Fragment , useState} from "react";
+import { Fragment , useReducer, useState} from "react";
 import defaultTodos from "../constants/defaultTodos";
 import "../index.css"
+import ACTION_NAMES from "../constants/actionNames";
+
+let { ADD_TODO , DELETE_TODO , DELETE_SELECTED_TODOS , MARK_AS_DONE} = ACTION_NAMES
+
+function reducer (todos , action) {
+    switch(action.type){
+        case ADD_TODO: 
+            return [...todos , {id: Math.random() , name: action.inputValue , CheckboxFlag: false}]
+        case DELETE_TODO: 
+            return todos.filter((todo) => todo.id !== action.id)
+        case MARK_AS_DONE: 
+            return todos.map((todo) => {
+                if(todo.id === action.id){
+                    return {...todo , CheckboxFlag: !action.CheckboxFlag}
+                }
+                return todo
+            })
+        case DELETE_SELECTED_TODOS: 
+            return todos.filter((todo) => !todo.CheckboxFlag)
+    }
+}
 
 function TodoList() {
 
-    let [ todos , setTodos ] = useState(defaultTodos)
+    let [ todos , dispatch ] = useReducer(reducer , defaultTodos)
     let [ inputValue , setInputValue ] = useState('')
 
     function handleInputValue(event){
         setInputValue(event.target.value)
+    }  
+
+    function handleCheckBox(todo){
+        dispatch({type: MARK_AS_DONE , id: todo.id , CheckboxFlag: todo.CheckboxFlag})
     }
 
-    function updateTodos() {
-        setTodos([...todos , {id: Math.random() , name: inputValue , CheckboxFlag: false}])
+    function deleteTodo(id){
+        dispatch({type: DELETE_TODO , id})
+    }   
+
+    function deleteSelectedTodos(){
+        dispatch({type: DELETE_SELECTED_TODOS})
+    }
+
+    function addTodo(){
+        dispatch({type: ADD_TODO , inputValue})
         setInputValue('')
     }
 
-    function toggleCheckboxFlag(id , CheckboxFlag){
-        setTodos(todos.map((todo) => {
-            if(todo.id === id){
-                return {...todo , CheckboxFlag: !CheckboxFlag}
-            }
-
-            return todo
-        }))
-    }
-
-    function deleteTodo(id) {
-        setTodos(todos.filter((todo) => todo.id !== id))
-    }    
-
-    function deleteSelected(){
-        setTodos(todos.filter((todo) => {
-            if(!todo.CheckboxFlag){
-                return todo
-            }
-        }))
-    }
 
     return <>
         <div className="Top"></div>
+        
         <div className="Todo-List">
             <div className="Top-Of-TodoList">
                 <div className="Top-Text">Todo List</div>
             </div>
+
             <div className="Bottom-Of-TodoList">
                 <div id="inputBar">
                     <label htmlFor="AddTodoInput" id="LabelAddTodoInput">Add a new task</label><br />
                     <input id='AddTodoInput'type="text" value={inputValue} onChange={handleInputValue}/>
-                    <button id="AddTodoBtn" onClick={updateTodos}>Add Todo</button>
+                    <button id="AddTodoBtn" onClick={addTodo}>Add Todo</button>
                 </div>
+                
                 <div className="TodosContainer">
                     {todos.map((todo) => {
-                        return <div className="todo-item">
-                            <input type="checkbox" className="IsDoneCheckbox" onChange={() => toggleCheckboxFlag(todo.id , todo.CheckboxFlag)}></input>
+                        return <div className="todo-item" key={todo.id}>
+                            <input type="checkbox" className="IsDoneCheckbox" onChange={() => handleCheckBox(todo)}></input>
                             <div className="todo-text">{todo.name}</div>
                             <button className="deleteBtn" onClick={() => deleteTodo(todo.id)}>x</button>
                         </div>
                     })}
-                    {todos.length ? <button id="deleteSelectedBtn" onClick={deleteSelected}>Delete Selected</button> : ''}
+
+                    {todos.length ? <button id="deleteSelectedBtn" onClick={deleteSelectedTodos}>Delete Selected</button> : ''}
                 </div>
                 
             </div>
